19efbbca5a2684276b18cf192ab7d0bb057093de
Vivek Shrestha <vvek.shrestha@gmail.com>
2025-09-27 15:27:36 -0400

    Clean workspace ready for production deployment

--- Latest_nepstate_Files/application/views/frontend/common/google_ads_box.php
+++
@@ -1,8 +0,0 @@
-<div class="ad-container mt-50" id="dummy-ad">
-				<img src="https://via.placeholder.com/1050x190" alt="Dummy Ad">
-			</div>
-
-            
-    <?php
-        // echo settings()->google_ads;
-    ?>
--- admin/app/helpers.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (! function_exists('get_category_title')) {
-    function get_category_title($slug) {
-        return DB::table('categories')->where('slug', $slug)->first()->title ?? '';
-    }
-}
