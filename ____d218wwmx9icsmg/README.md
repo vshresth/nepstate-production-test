022a1c296655278573deffa4d09d108b066f5cef
Vivek Shrestha <vvek.shrestha@gmail.com>
2025-09-27 18:02:39 -0400

    Cleanup: Remove temp folders from Git tracking

--- ____d218wwmx9icsmg/README.md
+++
@@ -1,2 +0,0 @@
-# Nepstate
-Listing platform for all kind of events, jobs, rooms &amp; rentals etc and developed on HTML, CSS and PHP CodeIgniter
--- ____d218wwmx9icsmg/application/views/frontend/common/breadcrum_dashboard.php
+++
@@ -1,8 +0,0 @@
-<section class="breadcrumbs-banner">
-   <div class="container">
-        <div class="breadcrumbs-area">
-            <h1 class="heading-title"><?php echo $title_page;?></h1>
-    	</div>
-   </div>
-</section>
-
